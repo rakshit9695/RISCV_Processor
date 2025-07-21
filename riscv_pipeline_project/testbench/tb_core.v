cb500ece64582d4737058d5c35909c85a780155d
rakshit9695 <126846527+rakshit9695@users.noreply.github.com>
2025-07-21 19:28:33 +0530

    added waveform code to tb

--- riscv_pipeline_project/testbench/tb_core.v
+++ riscv_pipeline_project/testbench/tb_core.v
@@ -1,4 +1,3 @@
-// Simple testbench for EDA Playground
 `timescale 1ns/1ps
 module tb_core;
     logic clk = 0, rst = 1;
@@ -10,8 +9,11 @@ module tb_core;
 
     // test program & stimulus
     initial begin
+        // VCD setup
+        $dumpfile("waveform.vcd");       // name of the dump file
+        $dumpvars(0, tb_core);           // dump all variables in tb_core and below
+
         // tiny program: x1=5; x2=7; x3 = x1 + x2; loop: beq x3,x3,loop
-        // assembled with objdump -> hex
         $readmemh("program.hex", DUT.imem.mem);
         #12 rst = 0;      // release reset after a few cycles
         #400 $finish;
